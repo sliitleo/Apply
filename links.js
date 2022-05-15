d315b41c2e4a0308220ba27fe352754230afd651
Thanura Manjitha Peiris <85317019+thxnura@users.noreply.github.com>
2022-05-15 18:02:44 +0530

    v6

--- links.js
+++
@@ -1,5 +0,0 @@
-var president = 'https://thxnura.com/';
-var vp = 'https://thxnura.com/vp';
-var secretary = 'https://thxnura.com/sec';
-var treasurer = 'https://thxnura.com/tre';
-var exco = 'https://thxnura.com/exco';
