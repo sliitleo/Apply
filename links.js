04956c1f81f3d18470e5683618b14536c58a0780
Thanura Manjitha Peiris <85317019+thxnura@users.noreply.github.com>
2022-05-19 19:27:05 +0530

    President

--- links.js
+++ links.js
@@ -1,4 +1,8 @@
 var president = 'https://docs.google.com/forms/d/e/1FAIpQLSciV3hXaJCcMQOImJwdQmHVJDtSFzMlP2WlgQ5oBIWyti4ibg/viewform?usp=sf_link';
+
+
+
+/**
 var vp = 'https://docs.google.com/forms/d/e/1FAIpQLSdyfD41Mefg_7nyLOe_jIa475gecfX0wZtIisrAzaLE5jzDVA/viewform?usp=sf_link';
 var secretary = 'https://docs.google.com/forms/d/e/1FAIpQLSerM_q7bz8WRjLM-UevTbn7TWPqEFMiOQIrf3Eju5nTuJst9g/viewform?usp=sf_link';
 var treasurer = 'https://docs.google.com/forms/d/e/1FAIpQLSfgw77dRKUg-pcTtRFKoLc9cuLUYWL7zQyz5E8FSi032C5d8g/viewform?usp=sf_linky';
@@ -9,3 +13,5 @@ var designer ="https://docs.google.com/forms/d/e/1FAIpQLScud0IOL8Q_Z4-GhM-iMb2Xk
 var photographer ="https://docs.google.com/forms/d/e/1FAIpQLSeKjJpQyIYm4xpUrTzsJW8RY0bvhEGVco5l1G3SluSISKM1mw/viewform?usp=sf_link";
 var content ="https://docs.google.com/forms/d/e/1FAIpQLSfJYXApMN9bewyoZ1QMgT7XAhVqpszQR_Zn0yu2uy6qhuwpQw/viewform?usp=sf_link";
 var presenter ="https://docs.google.com/forms/d/e/1FAIpQLSe9dCfPBcQJe9PUR3LJB-VXNKBFhEzRSry1fEM6FJtEET2OzA/viewform?usp=sf_link";
+
+ */
