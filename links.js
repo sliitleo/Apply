76bea8e1574b26a27530bf1babf6d186de1f7eef
Thanura Manjitha Peiris <85317019+thxnura@users.noreply.github.com>
2022-05-19 11:19:12 +0530

    all links added for audit

--- links.js
+++ links.js
@@ -1,6 +1,11 @@
 var president = 'https://docs.google.com/forms/d/e/1FAIpQLSciV3hXaJCcMQOImJwdQmHVJDtSFzMlP2WlgQ5oBIWyti4ibg/viewform?usp=sf_link';
-var vp = '#apply';
-var secretary = '#apply';
-var treasurer = '#apply';
-var exco = '#apply';
-var specialc = '#apply';
+var vp = 'https://docs.google.com/forms/d/e/1FAIpQLSdyfD41Mefg_7nyLOe_jIa475gecfX0wZtIisrAzaLE5jzDVA/viewform?usp=sf_link';
+var secretary = 'https://docs.google.com/forms/d/e/1FAIpQLSerM_q7bz8WRjLM-UevTbn7TWPqEFMiOQIrf3Eju5nTuJst9g/viewform?usp=sf_link';
+var treasurer = 'https://docs.google.com/forms/d/e/1FAIpQLSfgw77dRKUg-pcTtRFKoLc9cuLUYWL7zQyz5E8FSi032C5d8g/viewform?usp=sf_linky';
+var exco = 'https://docs.google.com/forms/d/e/1FAIpQLSf2Uz4uFhm01tfdiIn52fts_onA0WW9he8tUV8Ao64ZU5SiVw/viewform?usp=sf_link';
+var specialc = 'https://docs.google.com/forms/d/e/1FAIpQLSee-tNPlo3VXvrmgEZ1RCYhC6m8ZZz5elKBYWEZHsgyI6aIkA/viewform?usp=sf_link';
+
+var designer ="https://docs.google.com/forms/d/e/1FAIpQLScud0IOL8Q_Z4-GhM-iMb2XkQemDXHjXW7iC2EZUY8InhS5qQ/viewform?usp=sf_link";
+var photographer ="https://docs.google.com/forms/d/e/1FAIpQLSeKjJpQyIYm4xpUrTzsJW8RY0bvhEGVco5l1G3SluSISKM1mw/viewform?usp=sf_link";
+var content ="https://docs.google.com/forms/d/e/1FAIpQLSfJYXApMN9bewyoZ1QMgT7XAhVqpszQR_Zn0yu2uy6qhuwpQw/viewform?usp=sf_link";
+var presenter ="https://docs.google.com/forms/d/e/1FAIpQLSe9dCfPBcQJe9PUR3LJB-VXNKBFhEzRSry1fEM6FJtEET2OzA/viewform?usp=sf_link";
