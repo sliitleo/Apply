ad6e04ce813ffb1d02dc96adce8bfb89117008fd
Thanura Manjitha Peiris <85317019+thxnura@users.noreply.github.com>
2022-05-19 10:26:55 +0530

    President Link added

--- links.js
+++ links.js
@@ -1,4 +1,4 @@
-var president = '#';
+var president = 'https://docs.google.com/forms/d/e/1FAIpQLSciV3hXaJCcMQOImJwdQmHVJDtSFzMlP2WlgQ5oBIWyti4ibg/viewform?usp=sf_link';
 var vp = '#apply';
 var secretary = '#apply';
 var treasurer = '#apply';
