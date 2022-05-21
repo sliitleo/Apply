9da43bc649390591b5e0a828608589462381af81
Thanura Manjitha Peiris <85317019+thxnura@users.noreply.github.com>
2022-05-21 21:26:53 +0530

    Special Links added

--- links.js
+++ links.js
@@ -3,12 +3,18 @@ var vp = 'https://docs.google.com/forms/d/e/1FAIpQLSdyfD41Mefg_7nyLOe_jIa475gecf
 var secretary = 'https://docs.google.com/forms/d/e/1FAIpQLSerM_q7bz8WRjLM-UevTbn7TWPqEFMiOQIrf3Eju5nTuJst9g/viewform?usp=sf_link';
 var treasurer = 'https://docs.google.com/forms/d/e/1FAIpQLSfgw77dRKUg-pcTtRFKoLc9cuLUYWL7zQyz5E8FSi032C5d8g/viewform?usp=sf_linky';
 var exco = 'https://docs.google.com/forms/d/e/1FAIpQLSf2Uz4uFhm01tfdiIn52fts_onA0WW9he8tUV8Ao64ZU5SiVw/viewform?usp=sf_link';
+
+var specialc = 'https://docs.google.com/forms/d/e/1FAIpQLSee-tNPlo3VXvrmgEZ1RCYhC6m8ZZz5elKBYWEZHsgyI6aIkA/viewform?usp=sf_link';
+
+
+
 /**
 
 
 
 
-var specialc = 'https://docs.google.com/forms/d/e/1FAIpQLSee-tNPlo3VXvrmgEZ1RCYhC6m8ZZz5elKBYWEZHsgyI6aIkA/viewform?usp=sf_link';
+
+
 
 var designer ="https://docs.google.com/forms/d/e/1FAIpQLScud0IOL8Q_Z4-GhM-iMb2XkQemDXHjXW7iC2EZUY8InhS5qQ/viewform?usp=sf_link";
 var photographer ="https://docs.google.com/forms/d/e/1FAIpQLSeKjJpQyIYm4xpUrTzsJW8RY0bvhEGVco5l1G3SluSISKM1mw/viewform?usp=sf_link";
